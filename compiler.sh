0fde3f675b5295f5b30387ed3c2a61c796d74524
Michal Szajnecki <michal.szajnecki@gmail.com>
2019-05-05 13:02:01 +0200

    new separate folder for releases
    Created new folder for releases, because I need shortcut for dated releases

--- compiler.sh
+++ compiler.sh
@@ -2,5 +2,5 @@
 echo "Start compiler"
 now=$(date +'release_%d_%m_%Y_%H_%M')
 echo "Compiling in " $now
-mkdir $now
-./package-release.sh master $now --no-package
+mkdir releases/$now
+./package-release.sh master releases/$now --no-package
